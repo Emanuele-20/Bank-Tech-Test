726f4c581d72bb96cce739aaa1f31971120db36b
Emanuele Pace <emanuele.10@outlook.it>
2021-10-05 11:28:41 +0100

    Implemented 'withdrawal' method

--- src/bank.js
+++ src/bank.js
@@ -1,9 +1,13 @@
 class Bank {
 	constructor() {
-		this.balance = [];
+		this.balance = 0;
+		this.headStatement = "date || credit || debit || balance";
 	}
 	deposit(amount) {
-		this.balance.push(amount);
+		this.balance = this.balance + amount;
+	}
+	withdrawal(amount) {
+		this.balance = this.balance - amount;
 	}
 }
 module.exports = Bank;
--- test/bank.test.js
+++ test/bank.test.js
@@ -1,9 +1,23 @@
 const Bank = require("../src/bank");
 
+beforeEach(() => {
+	bank = new Bank();
+});
+
 describe("Bank Tech Test", () => {
 	it("Implements the possibility to make a deposit", () => {
-		let bank = new Bank();
 		bank.deposit(1000);
-		expect(bank.balance[0]).toBe(1000);
+		expect(bank.balance).toBe(1000);
+	});
+	it("Implements the possibility to make a deposit and update the balance", () => {
+		bank.deposit(1000);
+		bank.deposit(2000);
+		expect(bank.balance).toBe(3000);
+	});
+	it("Implements the possibility to make a withdrawal", () => {
+		bank.deposit(1000);
+		bank.deposit(2000);
+		bank.withdrawal(500);
+		expect(bank.balance).toBe(2500);
 	});
 });
